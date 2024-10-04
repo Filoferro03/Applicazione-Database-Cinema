ea4cc08dcacd5866d99ae4a4b76c3ef47b40ae3c
Filippo Ferretti <filippo.ferretti6@studio.unibo.it>
2024-10-04 11:50:56 +0200

    first implementation of view

--- app/src/main/java/appdatabase/View.java
+++ app/src/main/java/appdatabase/View.java
@@ -1,14 +1,22 @@
 package appdatabase;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class View extends Application{
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Login.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root, 400, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
 }
