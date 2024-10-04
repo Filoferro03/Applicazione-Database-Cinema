8f25a2ccffff9c08bab1a2f31995b8eb3b91d794
Filippo Ferretti <filippo.ferretti6@studio.unibo.it>
2024-10-04 13:40:28 +0200

    tried to make the app starts

--- app/src/main/java/appdatabase/App.java
+++ app/src/main/java/appdatabase/App.java
@@ -5,7 +5,7 @@ package appdatabase;
 
 import javafx.application.Application;
 
-public class Main {
+public class App {
 
     public static void main(String[] args) {
         Application.launch(View.class);
